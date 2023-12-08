d50c827eb78bcb3736a1f3d68799fad760bbdd6d
Giorgia Pazzi <72603458+giorgiapazzi@users.noreply.github.com>
2023-12-08 16:14:22 +0100

    Delete get_geometric_Jacobian.m

--- get_geometric_Jacobian.m
+++
@@ -1,5 +0,0 @@
-function J = get_geometric_Jacobian(q)
-robotModel = loadrobot("kukaIiwa14", DataFormat = 'column', Gravity=[0; 0; -9.81]);
-J = geometricJacobian(robotModel,q,"iiwa_link_ee");
-end
-
